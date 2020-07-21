cc4dff46d6ae6d7c41d92b4a9b2a0609e490453d
Radoslav Marinov <radoslav.k.marinov@gmail.com>
2020-07-21 17:56:12 +0300

    Add Products controller and routes

--- plugins/redmine_warehouse/init.rb
+++ plugins/redmine_warehouse/init.rb
@@ -1,8 +1,10 @@
 Redmine::Plugin.register :redmine_warehouse do
   name 'Redmine Warehouse plugin'
-  author 'Author name'
+  author 'Radoslav Marinov'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
+
+  menu :application_menu, :warehouse, { controller: 'products', action: 'index' }, caption: 'Warehouse'
 end
