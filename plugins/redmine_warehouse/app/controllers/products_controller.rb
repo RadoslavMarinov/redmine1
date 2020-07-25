eea679a7c96de20dee7c9a1ded1f74683a6862b6
Radoslav Marinov <radoslav.k.marinov@gmail.com>
2020-07-25 14:44:49 +0300

    Added new actions

--- plugins/redmine_warehouse/app/controllers/products_controller.rb
+++ plugins/redmine_warehouse/app/controllers/products_controller.rb
@@ -1,30 +1,52 @@
 class ProductsController < ApplicationController
-  before_action :find_project, :authorize, only: [:index]
+  before_action :find_project, :authorize, only: [:index, :create, :new, :edit, :update]
+  before_action :find_product, only: [:show, :edit, :update]
 
   def index
-    puts "\n\n>>>>>>>>>>>>>"
-    puts params
-    puts @project.inspect
-    puts "<<<<<<<<<<<<<\n\n"
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
+    #@product = Product.find(params[:id])
+  end
+
+  def edit
+
+    #@product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def update
+
+    puts params
+    @product.update(product_params)
+    flash[:notice] = l(:notice_successful_update)
+    redirect_to products_path(:project_id => @project)
   end
 
   def create
-    puts "\n\n CREATE \n\n"
-    puts "\n\n params: #{params} \n\n"
-    puts "\n\n product_params: #{product_params} \n\n"
+    # puts "!!!!!!!!!!!!!!!!!!!!"
+    # puts params[:product]["project_id"]
+    # puts "????????????????????"
+    # @project = Project.find(params[:product]["project_id"])
     product = Product.create(product_params)
-    redirect_to products_path
+    flash[:notice] = l(:notice_successful_create)
+    redirect_to products_path(:project_id =>  @project)
   end
 
 
   private
 
+  def find_product
+    @product = Product.find(params[:id])  
+  end
   def find_project
+    puts "\n\n FIND PROJECT>>>>>>>>>>>>"
+    puts params[:project_id]
+    puts "<<<<<<<<<<<<\n\n"
     # @project variable must be set before calling the authorize filter
     @project = Project.find(params[:project_id])
   end
--- plugins/redmine_warehouse/init.rb
+++ plugins/redmine_warehouse/init.rb
@@ -7,6 +7,10 @@ Redmine::Plugin.register :redmine_warehouse do
   author_url 'http://example.com/about'
 
   #permission :products, { products: [:index] }, public: true
-  permission :view_products, products: [:index, :show]
-  menu :project_menu , :products, { controller: 'products', action: 'index' }, caption: 'Warehouse'#, param: :project_id
+  permission :view_products, products: [:index, :show], public: true
+  permission :edit_products, products: :edit, public: true
+  permission :update_products, products: :update, public: true
+  permission :create_products, products: [:new, :create], public: true
+
+  menu :project_menu , :products, { controller: 'products', action: 'index' }, caption: 'Warehouse', param: :project_id
 end
